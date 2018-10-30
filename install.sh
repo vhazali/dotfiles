c2e0db49ec5fc34dee016c9d97f1120f48d36b2d
Victor Hazali <victorhazali@gmail.com>
2018-10-29 20:13:42 -0400

    Add checks to see if files are present before copying

--- install.sh
+++ install.sh
@@ -5,11 +5,18 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Set the terminal profile
-mv /etc/profile /etc/.originalprofile
+if [ -f /etc/profile ]
+    then
+        mv /etc/profile /etc/.orig_profile
+fi
 cp $DIR/profile /etc/profile
 
 # Install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Set the vimrc
+if [ -f ~/.vimrc]
+    then
+        mv ~/.vimrc ~/.orig_vimrc
+fi
 cp $DIR/vimrc ~/.vimrc
--- userInstall.sh
+++ userInstall.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-# Set the terminal profile
-# mv /etc/profile /etc/.originalprofile
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Set the bash profile
+if [ -f ~/.bash_profile ]
+    then
+        mv ~/.bash_profile ~/.orig_bash_profile
+fi
 cp $DIR/profile ~/.bash_profile
 
 # Install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Set the vimrc
+if [ -f ~/.vimrc]
+    then
+        mv ~/.vimrc ~/.orig_vimrc
+fi
 cp $DIR/vimrc ~/.vimrc
