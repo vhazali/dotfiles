f400dc8b88e4caa4c9ca084bf1f1065782487236
Victor Hazali <victorhazali@gmail.com>
2018-10-29 20:13:23 -0400

    Use variable to get current working directory. Gives more flexibility on where dotfiles folder is cloned

--- install.sh
+++ install.sh
@@ -2,12 +2,14 @@
 
 set -e
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Set the terminal profile
 mv /etc/profile /etc/.originalprofile
-cp ~/dotfiles/profile /etc/profile
+cp $DIR/profile /etc/profile
 
 # Install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Set the vimrc
-ln -s ~/dotfiles/vimrc ~/.vimrc
+cp $DIR/vimrc ~/.vimrc
--- userInstall.sh
+++ userInstall.sh
@@ -2,12 +2,14 @@
 
 # Set the terminal profile
 # mv /etc/profile /etc/.originalprofile
-cp ~/dotfiles/profile ~/.bash_profile
 set -e
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cp $DIR/profile ~/.bash_profile
 
 # Install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Set the vimrc
-ln -s ~/dotfiles/vimrc ~/.vimrc
+cp $DIR/vimrc ~/.vimrc
